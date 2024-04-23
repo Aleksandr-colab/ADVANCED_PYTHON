6003b15dbe7f34810f79d50b1fb757d269d48686
Aleksandr <122825144+Aleksandr-colab@users.noreply.github.com>
2024-04-23 22:27:27 +0300

    HW6

---
+++ chess.py
@@ -0,0 +1,13 @@
+
+# Напишите функцию в шахматный модуль. 
+# Используйте генератор случайных чисел для случайной расстановки ферзей в задаче выше. 
+# Проверяйте различный случайные варианты и выведите 4 успешных расстановки.
+def validate_queens(positions):
+    for i in range(8):
+        for j in range(i+1, 8):
+            # проверка на наличие на одной строке или диагонали
+            if positions[i] == positions[j] or \
+                positions[i] - i == positions[j] - j or \
+                positions[i] + i == positions[j] + j:
+                return False
+    return True
